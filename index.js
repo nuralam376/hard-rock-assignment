87d8b0130698cf89ecb8475a4afc32a80bc56b1d
Nur A Alam <nuraalam376@gmail.com>
2020-08-17 12:11:17 +0600

    Modified logics

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
+let singleLyrics = document.querySelector(".single-lyrics");
+let fancySearchResult = document.querySelector(".search-result");
+let simpleSearchResult = document.querySelector(".simple-result");
+
 // Handles the go back button of the lyrics
 const showSongsResults = () => {
-	document.querySelector(".single-lyrics").style.display = "none";
-	document.querySelector(".search-result").style.display = "block";
-	document.querySelector(".simple-result").style.display = "block";
+	singleLyrics.style.display = "none";
+	fancySearchResult.style.display = "block";
+	simpleSearchResult.style.display = "block";
 };
 
 // Displays the lyrics on the page
-const displayLyrics = (lyric, artist, title) => {
-	if (lyric.error) {
-		alert("Sorry, No lyric found for this song. Try for another one");
+const displayLyrics = (song, artist, title) => {
+	// Throw an alert if no lyrics is found
+	if (!song.lyrics) {
+		alert("Sorry, Lyrics is not found for this song. Try for another one");
 		return;
 	}
 
-	const singleLyric = document.querySelector(".single-lyrics");
-	singleLyric.innerHTML = `
+	singleLyrics.innerHTML = `
         <button class="btn go-back" onclick = "showSongsResults()">&lsaquo;</button>
         <h2 class="text-success mb-4">${title} - ${artist} </h2>
-        <pre class="lyric text-white">${lyric.lyrics}</pre>
+        <pre class="lyric text-white">${song.lyrics}</pre>
     `;
 
-	document.querySelector(".simple-result").style.display = "none";
-	document.querySelector(".search-result").style.display = "none";
-	singleLyric.style.display = "block";
+	simpleSearchResult.style.display = "none";
+	fancySearchResult.style.display = "none";
+	singleLyrics.style.display = "block";
+	window.scrollTo(0, 0);
 };
 
 // Fetch the the lyrics of the song
@@ -32,27 +37,10 @@ const fetchLyric = (artist, title) => {
 		.catch((error) => alert(error));
 };
 
-// Display the songs in the page
-const displaySongs = (song) => {
-	const songTitle = song.title;
-	const albumTitle = song.album.title;
-	const artistName = song.artist.name;
-
-	const simpleSongResults = document.querySelector(".simple-result");
-	const songResults = document.querySelector(".search-result");
-
-	const songInfo = document.createElement("div");
-	songInfo.className = "single-result row align-items-center my-3 p-3";
-	songInfo.innerHTML = `
-    <div class="col-md-9" id="songInfo">
-        <h3 class="lyrics-name">${songTitle}</h3>
-        <p class="author lead">Album: ${albumTitle} by <span>${artistName}</span></p>
-    </div>
-    <div class="col-md-3 text-md-right text-center" id="getLyrics">
-        <button onclick = "fetchLyric('${artistName}','${songTitle}')" class="btn btn-success">Get Lyrics</button>
-    </div>`;
-
+// Displays the lists in the simple design
+const simpleSongResults = (songTitle, albumTitle, artistName) => {
 	const simpleSong = document.createElement("div");
+	simpleSong.className = "text-center";
 	simpleSong.innerHTML = `
             <p class="author lead">
                 <strong>${albumTitle}</strong> Album by <span>${artistName}</span> 
@@ -62,19 +50,44 @@ const displaySongs = (song) => {
             </p>
     `;
 
-	simpleSongResults.appendChild(simpleSong);
-	songResults.appendChild(songInfo);
+	simpleSearchResult.appendChild(simpleSong);
 };
 
-// Handles the songs info
+// Displays the lists in the fancy design
+const fancySongResults = (songTitle, albumTitle, artistName) => {
+	const songInfo = document.createElement("div");
+	songInfo.className = "single-result row align-items-center my-3 p-3";
+	songInfo.innerHTML = `
+    <div class="col-md-9" id="songInfo">
+        <h3 class="lyrics-name">${songTitle}</h3>
+        <p class="author lead">${albumTitle} Album by <span>${artistName}</span></p>
+    </div>
+    <div class="col-md-3 text-md-right text-center" id="getLyrics">
+        <button onclick = "fetchLyric('${artistName}','${songTitle}')" class="btn btn-success">Get Lyrics</button>
+    </div>`;
+
+	fancySearchResult.appendChild(songInfo);
+};
+
+// Display the songs in the page
+const displaySongs = (song) => {
+	const songTitle = song.title;
+	const albumTitle = song.album.title;
+	const artistName = song.artist.name;
+
+	simpleSongResults(songTitle, albumTitle, artistName);
+	fancySongResults(songTitle, albumTitle, artistName);
+};
+
+// Handles the songs info after getting the result from the API
 const handleSongs = (songs) => {
-	if (songs.data < 0) {
+	if (!songs.data) {
 		alert("No Data found");
 		return;
 	}
 
-	document.querySelector(".simple-result").style.display = "block";
-	document.querySelector(".search-result").style.display = "block";
+	simpleSearchResult.style.display = "block";
+	fancySearchResult.style.display = "block";
 
 	for (let [index, song] of songs.data.entries()) {
 		// Shows only 10 items of the songs array
@@ -97,13 +110,15 @@ const fetchSongs = (songName) => {
 document.getElementById("search-song").addEventListener("click", function () {
 	const songName = document.getElementById("song-name").value;
 
-	document.querySelector(".single-lyrics").style.display = "none";
+	singleLyrics.style.display = "none";
 
-	if (songName.length == 0 || songName.trim() == "") {
+	// Checks whether the user types anything
+	if (!songName || songName.trim() == "") {
 		alert("Please enter the song name first");
+		return;
 	}
 
-	document.querySelector(".search-result").innerHTML = ""; // Clears the previous lists if any
-	document.querySelector(".simple-result").innerHTML = ""; // Clears the previous lists if any
+	simpleSearchResult.innerHTML = ""; // Clears the previous lists if any
+	fancySearchResult.innerHTML = ""; // Clears the previous lists if any
 	fetchSongs(songName);
 });
